354bb6bdb9a9ef487ddfec358ff65ad25583b9ae
Marina Moreira <mariidepazzi@gmail.com>
2019-04-09 20:56:44 -0300

    adicionando sistema + enum

--- src/clientes/Cliente.java
+++ src/clientes/Cliente.java
@@ -1,15 +1,33 @@
 package clientes;
 
+import App.Genero;
+
 public class Cliente {
 	private String name;
 	private String cpf;
 	private int age;
-	private String gender;
+	private Genero gender;
 	
-	public Cliente (String name,  String cpf, int age, String gender) {
+	public Cliente (String name,  String cpf, int age, Genero gender) {
 		this.name = name;
 		this.cpf = cpf;
 		this.age = age;
 		this.gender = gender;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public Genero getGender() {
+		return gender;
 	}
 }
